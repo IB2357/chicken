692e545ee535cca0ae2e1ae63a7e58af009d503c
ibrahim <ibrahim.off55555@gmail.com>
2025-04-27 16:20:53 +0300

    last commit before deleting the project from my hp laptop

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <nav>
         <!-- <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink> -->
-        <button @click="handleSignOut" v-if="isLoggedIn&&false">Sign Out</button>  <!--brocked for debuging-->
+        <button @click="handleSignOut" v-if="isLoggedIn && false">Sign Out</button> <!--brocked for debuging-->
       </nav>
     </div>
   </header>
@@ -22,12 +22,25 @@ import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
 const router = useRouter();
 const isLoggedIn = ref(false);
 let auth;
+const idToken = ref('');  // To store the ID token
 
 onMounted(() => {
   auth = getAuth();
   onAuthStateChanged(auth, (user) => {
     if (user) {
       isLoggedIn.value = true;
+
+
+      // Get the ID token
+      user.getIdToken(true).then((token) => {
+        idToken.value = token;  // Store the token
+        console.log('ID Token:', token);
+      }).catch((error) => {
+        console.error('Error fetching ID token:', error);
+      });
+
+
+
     } else {
       isLoggedIn.value = false;
     }
@@ -36,7 +49,7 @@ onMounted(() => {
 
 const handleSignOut = () => {
   signOut(auth).then(() => {
-    router.push('/sign-in');  
+    router.push('/sign-in');
   }).catch((error) => {
     console.error('Error during sign out:', error);
   });
